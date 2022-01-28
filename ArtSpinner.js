951c2cb9a6432999e8670ea67f7bfd48e0b7f774
Bernifal <94704789+Bernifal@users.noreply.github.com>
2022-01-28 13:37:06 +0000

    Add files via upload

---
+++ ArtSpinner.js
@@ -0,0 +1,35 @@
+
+const kickoff = () => {
+    const turnOn = document.querySelector(".turnon");
+    const wheel = document.querySelector(".wheel");
+    const startButton = document.querySelector(".spin");
+    let deg = 0;
+
+    startButton.addEventListener("click", () => {
+        startButton.style.pointerEvents = "none";
+
+        // test dif degrees below
+        deg = Math.floor(5000 + Math.random() * 5000)
+
+        wheel.style.transition = "all 10s ease-out";
+        wheel.style.transform = `rotate(${deg}deg)`;
+        wheel.classList.add("blur");
+    });
+
+    wheel.addEventListener("transitionend", () => {
+        wheel.classList.remove("blur");
+        startButton.style.pointerEvents = "auto";
+        wheel.style.transition = "none";
+        const actualDeg = deg % 360;
+        wheel.style.transform = `rotate(${actualDeg}deg)`;
+
+        // let turnOn = document.querySelector(".no");
+
+        turnOn.classList.add("confetti-on")
+
+        document.getElementById("congrats").innerText = "Congratulations!! 🎉"
+        document.getElementById("body").innerText = "You've won an artwork relating to this theme. Send a screenshot to Thaïs and await your options!"
+    });
+};
+
+window.addEventListener('DOMContentLoaded', kickoff, false);
