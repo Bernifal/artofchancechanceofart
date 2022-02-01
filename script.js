33f9ad4f06fbcbb19d44602be446fbde2461dada
Bernifal <94704789+Bernifal@users.noreply.github.com>
2022-02-01 18:46:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,33 @@ const kickoff = () => {
     const turnOn = document.querySelector(".turnon");
     const wheel = document.querySelector(".wheel");
     const startButton = document.querySelector(".spin");
+    const display = document.querySelector(".display")
+    
     let deg = 0;
+    let zoneSize = 40; // deg
+
+    const emojiZones = {
+        1: "You've won a Althorp Lamb painting! <br> Send a screenshot to Thaïs and await your options!",
+        2: "You've won a Barbican Walk painting! <br> Send a screenshot to Thaïs and await your options!",
+        3: "You've won a painting from the Misc category! <br> Send a screenshot to Thaïs and await your options!",
+        4: "You've won a Althorp Lamb painting! <br> Send a screenshot to Thaïs and await your options!",
+        5: "You've won a Barbican Walk painting! <br> Send a screenshot to Thaïs and await your options!",
+        6: "You've won a painting from the Misc category! <br> Send a screenshot to Thaïs and await your options!",
+        7: "You've won a Althorp Lamb painting! <br> Send a screenshot to Thaïs and await your options!",
+        8: "You've won a Barbican Walk painting! <br> Send a screenshot to Thaïs and await your options!",
+        9: "You've won a painting from the Misc category! <br> Send a screenshot to Thaïs and await your options!",
+    }
+
+    const handleWin = (actualDeg) => {
+        const winningSymbolNr = Math.ceil(actualDeg / zoneSize)
+        display.innerHTML = emojiZones[winningSymbolNr]
+    }
+
 
     startButton.addEventListener("click", () => {
+        
+        // display.innerHTML = ""
+
         startButton.style.pointerEvents = "none";
 
         // test dif degrees below
@@ -27,7 +51,10 @@ const kickoff = () => {
         turnOn.classList.add("confetti-on")
 
         document.getElementById("congrats").innerText = "Congratulations!! 🎉"
-        document.getElementById("body").innerHTML = `You've won an artwork relating to this theme. <br> Send a screenshot to Thaïs and await your options!`
+        // document.getElementById("body").innerHTML = `You've won an artwork relating to this theme. <br> Send a screenshot to Thaïs and await your options!`
+        
+        handleWin(actualDeg)
+   
     });
 };
 
